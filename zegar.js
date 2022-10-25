17cf05dad6b095575fe089ad1f1ce57909979024
Koleksiak <101290352+Koleksiak@users.noreply.github.com>
2022-10-25 07:31:30 +0200

    Update zegar.js

--- zegar.js
+++ zegar.js
@@ -1,7 +1,9 @@
 function displayTime(){
     var dateTime = new Date();
     var hrs = dateTime.getHours();
+    if (godzina<10) godziny ="0"+godziny;
     var min = dateTime.getMinutes();
+    if (godzina<10) minuty ="0"+minuty;
     var sec = dateTime.getSeconds();
 
     document.getElementById ('godziny').innerHTML = hrs;
